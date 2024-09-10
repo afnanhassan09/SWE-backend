d6aa281285e4abb149884c1a0bf5ea3cb3a061fa
Afnan Hassan <hassanafnan09@gmail.com>
2024-09-10 12:24:53 +0500

    Updated version

--- main.py
+++ main.py
@@ -25,10 +25,3 @@ async def process_url(request: UrlRequest):
     url = request.url
     summarized_text = generate_summary(url)
     return {"summary": summarized_text}
-
-
-if __name__ == "__main__":
-    port = int(
-        os.getenv("PORT", 8000)
-    )  # Use PORT environment variable or default to 8000
-    uvicorn.run(app, host="0.0.0.0", port=port)
