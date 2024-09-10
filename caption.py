74a89bbc6ef789822ec5dc754e92142284b38511
Afnan Hassan <hassanafnan09@gmail.com>
2024-09-10 15:08:22 +0500

    Updated version

--- caption.py
+++ caption.py
@@ -4,9 +4,7 @@ import re
 
 def extract_video_id(youtube_url):
     pattern = r'(?:https?://)?(?:www\.)?(?:youtube\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})'
-
     match = re.search(pattern, youtube_url)
-
     if match:
         return match.group(1)
     else:
@@ -14,13 +12,18 @@ def extract_video_id(youtube_url):
 
 
 def get_caption(youtube_link):
-
     video_id = extract_video_id(youtube_link)
-    # Fetch transcript
-    transcript = YouTubeTranscriptApi.get_transcript(video_id)
-    caption = ""
-    # Save transcript to a plain text file
-    for entry in transcript:
-        caption += entry["text"] + " "
+    if video_id is None:
+        raise ValueError("Invalid YouTube URL")
 
-    return caption
+    try:
+        # Fetch transcript
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        caption = ""
+        # Concatenate transcript entries
+        for entry in transcript:
+            caption += entry["text"] + " "
+        return caption
+    except Exception as e:
+        print(f"Error fetching transcript: {e}")
+        return None
